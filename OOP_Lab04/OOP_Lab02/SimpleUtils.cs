e0ffc5fb9a1cb5bbbfdc05bc182a2b44b8a01869
NickLatkovich <nicklatkovich@gmail.com>
2017-03-17 11:41:03 +0300

    OOP2_Lab04's SimpleUtils

---
+++ OOP_Lab04/OOP_Lab02/SimpleUtils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OOP_Lab02 {
+    class SimpleUtils {
+
+        static private Random rand = new Random( );
+
+        public static Int32 IRandom(Int32 maxValue) { return rand.Next( ) % (maxValue + 1); }
+
+        public static Int32 IRandomRange(Int32 minValue, Int32 maxValue) {
+            return minValue + IRandom(maxValue - minValue);
+        }
+
+    }
+}
