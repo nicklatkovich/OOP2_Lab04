37739bd87dea70918fc85c7edfac828766b56a27
NickLatkovich <nicklatkovich@gmail.com>
2017-03-17 11:41:28 +0300

    Demo

--- OOP_Lab04/Program.cs
+++ OOP_Lab04/Program.cs
@@ -1,6 +1,19 @@
-﻿namespace OOP_Lab04 {
+﻿using OOP_Lab02;
+using System;
+
+namespace OOP_Lab04 {
     class Program {
+
         static void Main(string[ ] args) {
+            CollectionType<Cylinder> ct = new CollectionType<Cylinder>( );
+            for (uint i = 0; i < 10; i++) {
+                ct.Add(Cylinder.Random);
+            }
+
+            foreach (Cylinder cylinder in ct) {
+                Console.WriteLine(cylinder);
+            }
         }
+
     }
 }
